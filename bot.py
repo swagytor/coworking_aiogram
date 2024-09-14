4fc601fb603bf7d186dc57d3719046900860529f
Swagytor <swagytor@yandex.ru>
2024-09-14 18:10:28 +0300

    Added handler routings and configs

--- bot.py
+++ bot.py
@@ -2,9 +2,13 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
 from aiogram.fsm.storage.redis import RedisStorage
+from aiogram import F
 
 import config
+from handlers import registration
+from states.registration import RegisterState
 
 
 async def start():
@@ -14,6 +18,13 @@ async def start():
     storage = RedisStorage.from_url(config.REDIS_URL)
 
     dp = Dispatcher(storage=storage)
+
+    dp.message.register(registration.start, CommandStart())
+
+    dp.message.register(registration.registration, RegisterState.registration)
+    dp.message.register(registration.coworking_auth, F.text == "Авторизоваться")
+    dp.message.register(registration.enter_email, RegisterState.enter_email)
+    dp.message.register(registration.enter_password, RegisterState.enter_password)
 
     try:
         await dp.start_polling(bot, skip_updates=True)
--- config.py
+++ config.py
@@ -6,3 +6,4 @@ dotenv.load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 REDIS_URL = os.getenv('REDIS_URL')
+BACKEND_URL = os.getenv('BACKEND_URL')
