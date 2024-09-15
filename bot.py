a77ca67c44246b0ca6c35840da57eeffdc45e039
Swagytor <swagytor@yandex.ru>
2024-09-15 10:25:10 +0300

    Added routing

--- bot.py
+++ bot.py
@@ -7,7 +7,9 @@ from aiogram.fsm.storage.redis import RedisStorage
 from aiogram import F
 
 import config
-from handlers import registration
+from handlers import registration, events, main_menu
+from states.events import EventState
+from states.main_menu import MainMenuState
 from states.registration import RegisterState
 
 
@@ -25,6 +27,12 @@ async def start():
     dp.message.register(registration.coworking_auth, F.text == "Авторизоваться")
     dp.message.register(registration.enter_email, RegisterState.enter_email)
     dp.message.register(registration.enter_password, RegisterState.enter_password)
+
+    dp.message.register(events.register_to_coworking, MainMenuState.main_menu, F.text == "Записаться в коворкинг")
+    dp.message.register(events.choose_coworking, EventState.choose_coworking)
+
+    dp.message.register(main_menu.change_auth_data, MainMenuState.change_auth_data,
+                        F.text == "Изменить авторизационные данные")
 
     try:
         await dp.start_polling(bot, skip_updates=True)
